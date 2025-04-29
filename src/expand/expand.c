d28424855100671abdd15fa3159bab85aeb56340
Valentin DELIERE <valentindeliere@gmail.com>
2025-04-29 16:55:39 +0200

    expander en chantier

---
+++ src/expand/expand.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 15:32:47 by valentin          #+#    #+#             */
+/*   Updated: 2025/04/29 15:52:44 by valentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char *expand_token(char *token)
+{
+    char    *res;
+    int     i;
+
+    res = malloc(sizeof(char *));
+    if (!res)
+        return (NULL);
+    i = 0;
+    while (token[i])
+    {
+        if (token[i] == '\'')
+            res = join_literal(res, token, &i);
+        else if (token[i] == '\"')
+            res = join_double_quote(res, token, &i);
+        else if (token[i] == '$')
+            res = join_dollar(res, token, &i);
+        else
+            res = join_char(res, token[i++]);
+    }
+    return (res);
+}
+
+void    expand_all_tokens(t_lexer *head)
+{
+    char    *new;
+    while (head)
+    {
+        new = expand_token(head->value);
+        if (!new)
+            return ;
+        free(head->value);
+        head->value = new;
+        head = head->next;
+    }
+}
---
+++ src/expand/join.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   join.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 15:32:18 by valentin          #+#    #+#             */
+/*   Updated: 2025/04/29 16:53:49 by valentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*join_literal(char *res, const char *token, int *i)
+{
+    int	start;
+
+    (*i)++;
+    start = *i;
+    while (token[*i] && token[*i] != '\'')
+    	(*i)++;
+    res = ft_strjoin(res, fr_substr(token, start, *i - start));
+    if (token [*i])
+    	(*i)++;
+    return (res);
+}
+
+char	*join_double_quote(char *res, const char *token, int *i)
+{
+	(*i)++;
+	while (token[*i] && token[*i] != '\"')
+	{
+		if (token[*i] == '$')
+			res = join_dollar(res, token, i);
+		else
+			res = join_char(res, token[(*i)++]);
+	}
+	if (token[*i])
+		(*i)++;
+	return (res);
+}
+
+char	*join_dollar(char *res, const char *token, int *i)
+{
+	char	*key;
+	char	*val;
+	int		start;
+
+	(*i)++;
+	// if (token[*i] == '?')
+	// {
+	// 	val = ft_itoa(g_exit_status)
+	// }
+	start = *i;
+	while (ft_isalnum(token[*i]) || token[*i] == '_')
+		(*i)++;
+	key = ft_substr(token, start, *i - start);
+	val
+}
